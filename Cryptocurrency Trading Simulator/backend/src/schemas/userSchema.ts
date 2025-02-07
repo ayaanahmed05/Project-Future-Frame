320de9ff270126166455f6dc59d26477ef2810f3
Focusucof <imthefirstfocus@gmail.com>
2025-02-06 20:18:21 -0500

    added user schema

---
+++ Cryptocurrency Trading Simulator/backend/src/schemas/userSchema.ts
@@ -0,0 +1,23 @@
+import { Schema, model } from 'mongoose';
+
+export const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    },
+    schemaVersion: {
+        type: Number,
+        required: true
+    }
+});
+
+export const User = model('User', userSchema);
